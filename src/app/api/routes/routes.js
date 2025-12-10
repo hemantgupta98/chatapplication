8951a4df164996cd2c5ad3062808dd11d9c3134d
HEMANT GUPTA <hemantgupta8800@gmail.com>
2025-12-10 19:35:42 +0530

    commit

---
+++ src/app/api/routes/routes.js
@@ -0,0 +1,20 @@
+import express from "expres";
+import User from "../../../../../../websocket/backend/models/user";
+import Chat from "../../../../../../websocket/backend/models/chat";
+
+const router = express.Router();
+
+// get all users (with online status)
+router.get("/user", async (req, res) => {
+  const user = await User.find({}, "-__v").sort({ username: 1 });
+  res.json(user);
+});
+
+// fetch messages by room id
+router.get("/chat/:room", async (req, res) => {
+  const { room } = req.params;
+  const chat = await Chat.find({ room }).sort({ created: 1 }).limit(200);
+  res.json(chat);
+});
+
+module.exports = router;
